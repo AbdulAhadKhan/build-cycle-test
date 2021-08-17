79887d42bf77761aa255de69a7196077ec4d81ce
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2021-08-17 17:10:29 +0500

    Things got out of hand. NASA hacked. Removing hacker script

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ function App() {
       <header className="App-header">
         <h1>Meep Moop</h1>
         <h2>This is React.</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* WARNING: This will hack NASA. DO NOT RUN! Please! */}
-        <code>
-          if <b>ice is cool</b> = boop <b>end</b>
-        </code>
       </header>
     </div>
   );
